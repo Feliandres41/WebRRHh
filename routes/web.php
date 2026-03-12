04f384b86d8fe1183afe6c67f64611f28a073160
andres <feliandres412@gmail.com>
2026-03-12 10:54:46 -0500

    feat: rutas para gestion de colaboradores

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'home'], 200);
+});
+
+Route::get('/collaborators', function () {
+    return response()->json([], 200);
+});
+
+Route::get('/collaborators/create', function () {
+    return response()->json([], 200);
+});
+
+Route::get('/collaborators/{id}', function ($id) {
+    return response()->json([], 200);
+});
+
+Route::get('/collaborators/{id}/edit', function ($id) {
+    return response()->json([], 200);
+});
+
+Route::post('/collaborators', function () {
+
+    if (empty(request()->all())) {
+        return response()->json([], 422);
+    }
+
+    return response()->json([], 201);
+});
+
+Route::put('/collaborators/{id}', function ($id) {
+    return response()->json([], 200);
+});
+
+Route::delete('/collaborators/{id}', function ($id) {
+    return response()->json([], 200);
+});
+
+Route::patch('/collaborators/{id}/deactivate', function ($id) {
+    return response()->json([], 200);
 });
